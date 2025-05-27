2add70af2a7a5db5f9df4b3841571d621a80f57d
uraneko <BOOTHAlessandra@tutanota.com>
2025-05-27 13:49:31 +0100

    expose manifest reader and help reader through crate resolver

--- jagaimo_macro/src/read_manifest.rs
+++ jagaimo_macro/src/read_manifest.rs
@@ -4,42 +4,75 @@ use std::fs::read_to_string;
 // this is crate wide public so that
 // if I needed to get the manifest and reuse it multiple times,
 // i would use this with ref returning fns
-pub(crate) struct CrateResolver {
+pub(crate) struct ResolveCrate {
     // running in workspace or not
     // is_workspace: bool,
     // name of the crate being compiled
-    // who: String,
+    who: String,
     // manifest of the compile_who crate
-    manifest: String,
+    // manifest: String,
 }
 
-impl CrateResolver {
-    fn resolve() -> Self {
+impl ResolveCrate {
+    pub fn new() -> Self {
         let manifest = std::fs::read_to_string("Cargo.toml").unwrap();
         if manifest.contains("[workspace]") {
-            let compile_who = std::env::var("CARGO_CRATE_NAME").unwrap();
-            let manifest = std::fs::read_to_string(compile_who + "/Cargo.toml").unwrap();
-            Self { manifest }
+            let who = std::env::var("CARGO_CRATE_NAME").unwrap();
+            Self { who }
         } else {
-            Self { manifest }
+            Self {
+                who: ".".to_owned(),
+            }
         }
     }
 
-    fn into_manifest(self) -> String {
-        self.manifest
+    pub fn read_manifest(self) -> ReadManifest {
+        self.into()
     }
 
-    fn as_manifest(&self) -> &str {
-        &self.manifest
+    pub fn read_help(self) -> ReadHelp {
+        self.into()
+    }
+
+    fn who(&self) -> &str {
+        &self.who
     }
 }
 
-pub(crate) struct ManifestReader;
+pub struct ReadHelp {
+    help: String,
+}
 
-impl ManifestReader {
-    fn crate_name_version() -> [String; 2] {
-        let manifest = CrateResolver::resolve().into_manifest();
-        let mut iter = manifest
+impl From<ResolveCrate> for ReadHelp {
+    fn from(resolver: ResolveCrate) -> Self {
+        Self {
+            help: std::fs::read_to_string(resolver.who + "/help.toml").unwrap(),
+        }
+    }
+}
+
+impl ReadHelp {
+    fn into_table(self) -> toml::Table {
+        self.help.parse::<toml::Table>().unwrap()
+    }
+}
+
+pub(crate) struct ReadManifest {
+    manifest: String,
+}
+
+impl From<ResolveCrate> for ReadManifest {
+    fn from(resolver: ResolveCrate) -> Self {
+        Self {
+            manifest: std::fs::read_to_string(resolver.who + "/Cargo.toml").unwrap(),
+        }
+    }
+}
+
+impl ReadManifest {
+    pub fn crate_name_version(&self) -> [String; 2] {
+        let mut iter = self
+            .manifest
             .lines()
             .filter(|l| l.starts_with("name = ") || l.starts_with("version = "))
             .map(|l| l.to_owned());
@@ -47,9 +80,9 @@ impl ManifestReader {
         [iter.next().unwrap(), iter.next().unwrap()]
     }
 
-    pub(crate) fn crate_name() -> String {
-        let manifest = CrateResolver::resolve().into_manifest();
-        let mut iter = manifest
+    pub(crate) fn crate_name(&self) -> String {
+        let mut iter = self
+            .manifest
             .lines()
             .filter(|l| l.starts_with("name = ") || l.starts_with("version = "))
             .map(|l| l.to_owned());
@@ -57,9 +90,9 @@ impl ManifestReader {
         iter.next().unwrap()
     }
 
-    pub(crate) fn crate_version() -> String {
-        let manifest = CrateResolver::resolve().into_manifest();
-        let mut iter = manifest
+    pub(crate) fn crate_version(&self) -> String {
+        let mut iter = self
+            .manifest
             .lines()
             .filter(|l| l.starts_with("name = ") || l.starts_with("version = "))
             .map(|l| l.to_owned());
