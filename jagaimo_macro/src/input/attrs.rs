c9af585b4528c2b45969285a028954d9651b974b
uraneko <BOOTHAlessandra@tutanota.com>
2025-06-02 18:56:45 +0100

    add macro mock attributes parsing

---
+++ jagaimo_macro/src/input/attrs.rs
@@ -0,0 +1,122 @@
+use proc_macro2::Span;
+use syn::Token;
+use syn::ext::IdentExt;
+use syn::parse::{Parse, ParseStream, Result as PRes};
+use syn::{Ident, Lit};
+use syn::{bracketed, parenthesized};
+
+use crate::resolve_crate::ResolveCrate;
+
+pub fn enforce_nc(name: &mut String) {
+    name.replace_range(
+        0..1,
+        &name.get(0..1).map(|s| s.to_ascii_uppercase()).unwrap(),
+    );
+
+    while let Some(idx) = name.find('_') {
+        let next = name.get(idx + 1..idx + 2);
+        if let Some(s) = next {
+            name.replace_range(idx..idx + 2, &s.to_ascii_uppercase());
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Attrs {
+    root_name: String,
+    help: bool,
+    version: bool,
+    nu_completions: bool,
+    fish_completions: bool,
+    ignore_naming_conventions: bool,
+    auto_alias: bool,
+    derives: Vec<Ident>,
+}
+
+impl Default for Attrs {
+    fn default() -> Self {
+        Self {
+            root_name: ResolveCrate::new().read_manifest().crate_name().to_string(),
+            help: true,
+            version: true,
+            nu_completions: false,
+            fish_completions: false,
+            ignore_naming_conventions: false,
+            auto_alias: true,
+            derives: Self::default_derives(),
+        }
+    }
+}
+
+impl Attrs {
+    fn default_derives() -> Vec<Ident> {
+        vec![
+            Ident::new("Debug", Span::call_site()),
+            Ident::new("PartialEq", Span::call_site()),
+            Ident::new("Clone", Span::call_site()),
+        ]
+    }
+}
+
+impl Parse for Attrs {
+    fn parse(stream: ParseStream) -> PRes<Self> {
+        let mut attrs = Attrs::default();
+        // NOTE this should consume ZERO tokens if the user provided no attributes
+
+        if !stream.peek(Token![#]) {
+            return Ok(attrs);
+        }
+
+        _ = <Token![#]>::parse(stream)?;
+        let content;
+        let _brackets = bracketed!(content in stream);
+        // WARN doesnt work
+        // cus not all attrs are boolean
+        while content.peek(Ident::peek_any) {
+            match &Ident::parse(&content)?.to_string()[..] {
+                // "fish_cmp"
+                // | "nu_cmp"
+                // | "ignore_naming_conventions"
+                // | "no_help"
+                // | "no_version" => unimplemented!(),
+                "root_name" => {
+                    _ = <Token![=]>::parse(&content)?;
+                    let name = Lit::parse(&content)?;
+                    let Lit::Str(ls) = name else {
+                        unreachable!("root_name attr has to take a single str lit")
+                    };
+
+                    attrs.root_name = ls.value();
+                }
+                "fish_cmp" => attrs.fish_completions = true,
+                "nu_cmp" => attrs.nu_completions = true,
+                "no_help" => attrs.help = false,
+                "no_version" => attrs.version = false,
+                "ignore_naming_conventions" => attrs.ignore_naming_conventions = true,
+                "no_auto_alias" => attrs.auto_alias = false,
+                "derives" => {
+                    let derives;
+                    let _parens = parenthesized!(derives in content);
+                    attrs.derives = std::mem::take(
+                        &mut derives
+                            .parse_terminated(Ident::parse, Token![,])?
+                            .into_iter()
+                            .collect(),
+                    );
+                }
+                val => panic!("unrecognized mock attribute {}", val),
+            }
+
+            if !content.is_empty() {
+                _ = <Token![,]>::parse(&content)?;
+            }
+        }
+        // TODO dont capitalize any type name when this flag is on
+        if !attrs.ignore_naming_conventions {
+            // TODO all type tree type idents need this
+            enforce_nc(&mut attrs.root_name);
+        }
+
+        Ok(attrs)
+    }
+}
