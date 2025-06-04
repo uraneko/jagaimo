3eef926cd0a187e7fea746744996aa9811aeb24a
uraneko <BOOTHAlessandra@tutanota.com>
2025-06-04 15:50:48 +0100

    add a few command rules to macro call in jagaimo/src/lib.rs that, once expanded, showcase why operations name resolution is required

--- jagaimo/src/lib.rs
+++ jagaimo/src/lib.rs
@@ -21,6 +21,7 @@ jagaimo! {
     c { s(history) o(list) [ max<u8> verbose tags<Vec<String>> ] }
     c { [ <(String, f64)> size<Dimensions> show_all ] }
     c { s(collections) o(obfuscate) [ <std::fs::File> allocate  rand<f64> hash<String> fuzzing algorithm<String> ] }
+    c { s(collections) o(list) [ <Vec<f64>> long pipe_into_list_of<String> output_file<std::fs::File> ] }
     // t { s(colls) o(list) |_ base: String|
     //         { if base == "_" { auto as bool } else { base "BASE{base}" as Base } }
     // }
